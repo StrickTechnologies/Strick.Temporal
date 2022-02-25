1d6aeae587588219937fa42823ba68129b39fed1
Jim Strickland <79662634+JimStrickland@users.noreply.github.com>
2022-02-25 11:42:15 -0500

    Unit tests for TemporalComparerColumnList.AddRange method

--- Strick.Temporal.Test/TemporalComparerColumnListTests.cs
+++ Strick.Temporal.Test/TemporalComparerColumnListTests.cs
@@ -47,7 +47,47 @@ namespace Strick.Temporal.Test
 		[TestMethod]
 		public void AddRange()
 		{
-			//todo:write unit tests here...
+			TemporalComparer tc = new TemporalComparer(EmployeeTestData.getDataTable());
+
+			Assert.IsNotNull(tc);
+			Assert.IsNotNull(tc.Table);
+
+			var cols = tc.IncludedColumns;
+			Assert.IsNotNull(cols);
+			Assert.AreEqual(0, cols.Count);
+
+			tc.IncludedColumns.AddRange(new[] { "ID" });
+			Assert.AreEqual(1, cols.Count);
+
+			tc.IncludedColumns.Clear();
+			Assert.IsNotNull(cols);
+			Assert.AreEqual(0, cols.Count);
+
+			tc.IncludedColumns.AddRange(new[] { "ID", "Name" });
+			Assert.AreEqual(2, cols.Count);
+
+			tc.IncludedColumns.Clear();
+			Assert.IsNotNull(cols);
+			Assert.AreEqual(0, cols.Count);
+
+			Assert.ThrowsException<ArgumentException>(() => tc.IncludedColumns.AddRange(new[] { "bogus" }));
+			Assert.IsNotNull(cols);
+			Assert.AreEqual(0, cols.Count);
+
+			Assert.ThrowsException<ArgumentException>(() => tc.IncludedColumns.AddRange(new[] { "ID", "bogus" }));
+			Assert.IsNotNull(cols);
+			//Note: the ID column gets added before it hits the bogus column.
+			//      Not sure if this is how it *should* work or not, but this test checks that it's working the way it's coded.
+			//      If this is ever determined to be a bug, or something we want to change, this unit test will obviously have to change
+			Assert.AreEqual(1, cols.Count);
+
+			tc.IncludedColumns.Clear();
+			Assert.IsNotNull(cols);
+			Assert.AreEqual(0, cols.Count);
+			
+			Assert.ThrowsException<ArgumentException>(() => tc.IncludedColumns.AddRange(new[] { "bogus", "ID" }));
+			Assert.IsNotNull(cols);
+			Assert.AreEqual(0, cols.Count);
 		}
 	}
 }
